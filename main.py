78da03de2091294245fe24655ea405dd50916983
Moritz Schalle <schallemoritz188@gmail.com>
2025-04-06 19:14:22 +0200

    added player in the middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,12 +8,15 @@ def main():
     pygame_clock = pygame.time.Clock()
     dt = 0
 
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = pygame_clock.tick(60)/1000
 
